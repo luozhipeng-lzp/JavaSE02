68e2f785113eb12ba76eca6e9a6d9de266ff108f
luozhipeng-lzp <2657750679@qq.com>
2020-06-21 23:26:25 +0800

    练习

--- javaSE-2/src/com/study/demo/Hospital.java
+++ javaSE-2/src/com/study/demo/Hospital.java
@@ -10,7 +10,7 @@ public class Hospital {
 
 	public static void main(String[] args) {
 		
-		String idcard = "650214199001015988";
+		String idCard = "650214199001015988";
 		String name = "王三";
 		String mobile = "17794256337";
 		String visitType = "社保卡";
@@ -18,13 +18,13 @@ public class Hospital {
 		String doctor = "终南山";
 		String date = "20200621";
 		double cost = 15.00;
-		String expensetype = "专家门诊";
-		int ordernumber = 01;
-		String paymode = "微信支付";
+		String expenseType = "专家门诊";
+		int orderNumber = 01;
+		String payMode = "微信支付";
 		String warning = "弃诊超过三次将取消优先出号！";
 		
 		System.out.println("天水三院");
-		System.out.println("身份证号码:"+idcard);
+		System.out.println("身份证号码:"+idCard);
 		System.out.println("姓名:"+name);
 		System.out.println("电话号码:"+mobile);
 		System.out.println("就诊类型:"+visitType);
@@ -32,9 +32,9 @@ public class Hospital {
 		System.out.println("医生:"+doctor);
 		System.out.println("就诊日期:"+date);
 		System.out.println("挂号费:"+cost);
-		System.out.println("费用类型:"+expensetype);
-		System.out.println("就诊号:"+ordernumber);
-		System.out.println("支付方式:"+paymode);
+		System.out.println("费用类型:"+expenseType);
+		System.out.println("就诊号:"+orderNumber);
+		System.out.println("支付方式:"+payMode);
 		System.out.println();
 		System.out.println("--------挂号成功--------");
 		System.out.println("警示:"+warning);
